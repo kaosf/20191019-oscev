66f24af8e7e31e39371c4def381979bc027c9091
ka <ka.kaosf@gmail.com>
2019-09-28 15:42:24 +0900

    Make election/candidate nest

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,34 +1,35 @@
 class CandidatesController < ApplicationController
+  before_action :set_election
   before_action :set_candidate, only: [:show, :edit, :update, :destroy]
 
-  # GET /candidates
-  # GET /candidates.json
+  # GET /elections/:election_id/candidates
+  # GET /elections/:election_id/candidates.json
   def index
-    @candidates = Candidate.all
+    @candidates = @election.candidates
   end
 
-  # GET /candidates/1
-  # GET /candidates/1.json
+  # GET /elections/:election_id/candidates/1
+  # GET /elections/:election_id/candidates/1.json
   def show
   end
 
-  # GET /candidates/new
+  # GET /elections/:election_id/candidates/new
   def new
-    @candidate = Candidate.new
+    @candidate = @election.candidates.build
   end
 
-  # GET /candidates/1/edit
+  # GET /elections/:election_id/candidates/1/edit
   def edit
   end
 
-  # POST /candidates
-  # POST /candidates.json
+  # POST /elections/:election_id/candidates
+  # POST /elections/:election_id/candidates.json
   def create
-    @candidate = Candidate.new(candidate_params)
+    @candidate = @election.candidates.build(candidate_params)
 
     respond_to do |format|
       if @candidate.save
-        format.html { redirect_to @candidate, notice: 'Candidate was successfully created.' }
+        format.html { redirect_to election_candidate_url(@election, @candidate), notice: 'Candidate was successfully created.' }
         format.json { render :show, status: :created, location: @candidate }
       else
         format.html { render :new }
@@ -37,8 +38,8 @@ class CandidatesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /candidates/1
-  # PATCH/PUT /candidates/1.json
+  # PATCH/PUT /elections/:election_id/candidates/1
+  # PATCH/PUT /elections/:election_id/candidates/1.json
   def update
     respond_to do |format|
       if @candidate.update(candidate_params)
@@ -51,20 +52,24 @@ class CandidatesController < ApplicationController
     end
   end
 
-  # DELETE /candidates/1
-  # DELETE /candidates/1.json
+  # DELETE /elections/:election_id/candidates/1
+  # DELETE /elections/:election_id/candidates/1.json
   def destroy
     @candidate.destroy
     respond_to do |format|
-      format.html { redirect_to candidates_url, notice: 'Candidate was successfully destroyed.' }
+      format.html { redirect_to election_candidates_url(@election), notice: 'Candidate was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
+    def set_election
+      @election = Election.find(params[:election_id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_candidate
-      @candidate = Candidate.find(params[:id])
+      @candidate = @election.candidates.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/election.rb
+++ app/models/election.rb
@@ -1,2 +1,3 @@
 class Election < ApplicationRecord
+  has_many :candidates, dependent: :destroy
 end
