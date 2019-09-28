dba32185dd7a5a10c0a42a0772d7b463b139fdcd
ka <ka.kaosf@gmail.com>
2019-09-28 14:31:21 +0900

    Add NOT NULL

--- db/migrate/20190928053059_create_candidates.rb
+++ db/migrate/20190928053059_create_candidates.rb
@@ -2,8 +2,8 @@ class CreateCandidates < ActiveRecord::Migration[6.0]
   def change
     create_table :candidates do |t|
       t.references :election, null: false, foreign_key: true
-      t.integer :number
-      t.string :name
+      t.integer :number, null: false
+      t.string :name, null: false
 
       t.timestamps
     end
