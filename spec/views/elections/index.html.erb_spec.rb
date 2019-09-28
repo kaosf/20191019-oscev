2fd1420fda32a70b0f41271051345f4a6e21c277
ka <ka.kaosf@gmail.com>
2019-09-28 13:41:06 +0900

    Fix elections#index view spec

--- spec/views/elections/index.html.erb_spec.rb
+++ spec/views/elections/index.html.erb_spec.rb
@@ -4,25 +4,24 @@ RSpec.describe "elections/index", type: :view do
   before(:each) do
     assign(:elections, [
       Election.create!(
-        :l => "9.99",
-        :m => "9.99",
-        :a => "9.99",
-        :b => "9.99"
+        :l => 12480,
+        :m => 15342,
+        :a => 1,
+        :b => 1
       ),
       Election.create!(
-        :l => "9.99",
-        :m => "9.99",
-        :a => "9.99",
-        :b => "9.99"
+        :l => 12480,
+        :m => 15342,
+        :a => 1,
+        :b => 1
       )
     ])
   end
 
   it "renders a list of elections" do
     render
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
+    assert_select "tr>td", :text => "12480".to_s, :count => 2
+    assert_select "tr>td", :text => "15342".to_s, :count => 2
+    assert_select "tr>td", :text => "1".to_s, :count => 4
   end
 end
