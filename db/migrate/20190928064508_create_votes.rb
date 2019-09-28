6f863ea5e6e45c724dd22da7aadc5f5f1d241a3b
ka <ka.kaosf@gmail.com>
2019-09-28 15:45:28 +0900

    Add NOT NULL

--- db/migrate/20190928064508_create_votes.rb
+++ db/migrate/20190928064508_create_votes.rb
@@ -2,8 +2,8 @@ class CreateVotes < ActiveRecord::Migration[6.0]
   def change
     create_table :votes do |t|
       t.references :election, null: false, foreign_key: true
-      t.decimal :m
-      t.decimal :r
+      t.decimal :m, null: false
+      t.decimal :r, null: false
 
       t.timestamps
     end
