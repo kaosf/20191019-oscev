d2e651e2cec156658e9487cc2054ece5501a7ea2
ka <ka.kaosf@gmail.com>
2019-09-28 13:40:57 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_28_043939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "elections", force: :cascade do |t|
+    t.decimal "l", null: false
+    t.decimal "m", null: false
+    t.decimal "a", null: false
+    t.decimal "b", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
