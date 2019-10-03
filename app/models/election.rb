8aaafbee0169999f72f85e3c55e55f136321ea46
ka <ka.kaosf@gmail.com>
2019-10-04 02:56:04 +0900

    Implement Election#set_n_sq and call it before validation

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,4 +1,10 @@
 class Election < ApplicationRecord
   has_many :candidates, dependent: :destroy
   has_many :votes, dependent: :destroy
+
+  before_validation :set_n_sq
+
+  def set_n_sq
+    self.n_sq = self.n ** 2
+  end
 end
--- spec/models/election_spec.rb
+++ spec/models/election_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Election, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#set_n_sq' do
+    let(:election) { FactoryBot.build :election, n_sq: nil }
+
+    it do
+      election.set_n_sq
+      expect(election.n_sq).to eq(election.n ** 2)
+    end
+  end
+
+  describe 'before_validation :set_n_sq' do
+    let(:election) { FactoryBot.build :election }
+    before { expect(election).to receive(:set_n_sq) }
+    it { election.valid? }
+    it { election.save }
+    it { election.update b: 10 }
+  end
 end
