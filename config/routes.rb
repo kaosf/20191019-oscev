402cf4623a1dd8c524dd61be9de62aee0457f96b
ka <ka.kaosf@gmail.com>
2019-10-18 19:16:59 +0900

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :ballots
   root to: 'public/elections#index'
   namespace :public do
     resources :elections, only: %i(index show)
   end
 
   resources :elections do
+    resources :ballots, only: %i(new)
     resources :candidates
     resources :votes
     resources :results, only: %i(index)
